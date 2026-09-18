173fba53159ce8ef0c243e3a51378e177f51203b
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2026-09-17 21:39:41 -0400

    checks/modules: Ignore `system.build` when not `enable`-gated
    The `image.repart` module couldn't *just* be added without causing
    changes in the configuration. It would always end-up adding an
    `image` attribute to `system.build`.
    
    With this, we are (1) checking the feature (`.enable`) first, and only
    when missing we judge it's fine that `system.build` is changed.

--- checks/modules.nix
+++ checks/modules.nix
@@ -4,9 +4,30 @@
 }:
 
 let
+  inherit (pkgs) lib;
+  # `system.build.image` is unconditionally set in older releases.
+  # See: https://github.com/NixOS/nixpkgs/pull/561557
+  # Skip `system.build` only when the `enable` option is not found.
+  skipBuildRepart =
+    !(
+      (pkgs.nixos (
+        { modulesPath, ... }:
+        {
+          imports = [ "${modulesPath}/image/repart.nix" ];
+        }
+      )).options.image.repart or { } ? enable
+    );
   check = import ../lib/check-modules-no-ops.nix {
     inherit pkgs;
     modules = nixosModules;
+    ignoreChangesIn = {
+      image = lib.optionals skipBuildRepart [
+        [
+          "system"
+          "build"
+        ]
+      ];
+    };
   };
 in
 builtins.seq check.result (
