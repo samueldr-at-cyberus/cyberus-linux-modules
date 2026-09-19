2aef5e589e5873bb81f958969db1262bc95e39cd
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2026-09-18 22:43:04 -0400

    checks/image: Use extendModules for the inner image
    This doesn't solve the main issue (having to bundle the image), but this
    is handling the wishlist item.

--- checks/image.nix
+++ checks/image.nix
@@ -69,58 +69,66 @@ rec {
     '';
   };
 
-  imageUpdates =
-    let
-      # Generate an update bundle. It would be nice to use extendModules
-      # somehow...
-      updateBundle =
-        (mkImageTest {
-          name = "New Version";
+  imageUpdates = mkImageTest {
+    name = "Image Update Test";
 
-          additionalConfig = {
-            cyberus-linux.image.version = "1.0.1";
+    additionalConfig =
+      {
+        lib,
+        config,
+        extendModules,
+        ...
+      }:
+      {
+        cyberus-linux.image.version = "1.0.0";
+        # FIXME: The shared directory is not mounted, so we cannot use copy_from_host.
+        # We work around this by bundling the image in the generated image.
+        environment.etc.updates =
+          # Only bundle the image in the "outer" system.
+          # If we don't, this `extendModules` also would include an image,
+          # which also would [...] leading to an infinite recursion.
+          lib.mkIf (config.cyberus-linux.image.version == "1.0.0") {
+            source =
+              (extendModules {
+                modules = [
+                  {
+                    cyberus-linux.image.version = lib.mkForce "1.0.1";
+                  }
+                ];
+              }).config.system.build.imageUpdateBundle;
           };
-        }).nodes.machine.system.build.imageUpdateBundle;
-    in
-    mkImageTest {
-      name = "Image Update Test";
-
-      # See the TODO below.
-      additionalConfig = {
-        environment.etc.updates = {
-          source = updateBundle;
-        };
       };
 
-      testScript = ''
-        machine.succeed("mkdir -p /var/updates")
+    testScript = ''
+      machine.succeed("mkdir -p /var/updates")
 
-        # TODO The shared directory is not mounted, so we cannot use copy_from_host.
-        machine.succeed("cp -v /etc/updates/* /var/updates/")
+      # Make the bundled image available.
+      machine.succeed("rm -rf /var/updates")
+      machine.succeed("ln -sf /etc/updates /var/updates")
 
-        current_version = machine.succeed("grep IMAGE_VERSION /etc/os-release")
-        t.assertIn("1.0.0", current_version)
+      current_version = machine.succeed("grep IMAGE_VERSION /etc/os-release")
+      t.assertIn("1.0.0", current_version)
 
-        updates = machine.succeed("updatectl check")
-        t.assertIn("1.0.0 → 1.0.1", updates)
+      updates = machine.succeed("updatectl check")
+      t.assertIn("1.0.0 → 1.0.1", updates)
 
-        # Ensure the update process has completed running.
-        # NOTE: This command likely may not fail on update failures.
-        machine.succeed("updatectl update")
+      # Ensure the update process has completed running.
+      # NOTE: This command likely may not fail on update failures.
+      machine.succeed("updatectl update")
 
-        # For some failure modes, the `updatectl update` command will exit(0)
-        # Additionally, it will print confusing output such as:
-        #     host@1.0.1: ✗ No space left on device
-        #     host@1.0.1: ✓ Already up-to-date
-        # So we need to check the `check` subcommand instead.
-        # This saves a needless reboot in case of failures.
-        output = machine.succeed("updatectl check 2>&1")
-        t.assertIn("No updates available.", output)
+      # For some failure modes, the `updatectl update` command will exit(0)
+      # Additionally, it will print confusing output such as:
+      #     host@1.0.1: ✗ No space left on device
+      #     host@1.0.1: ✓ Already up-to-date
+      # So we need to check the `check` subcommand instead.
+      # This saves a needless reboot in case of failures.
+      output = machine.succeed("updatectl check 2>&1")
+      t.assertIn("No updates available.", output)
 
-        machine.reboot()
+      machine.reboot()
 
-        current_version = machine.succeed("grep IMAGE_VERSION /etc/os-release")
-        t.assertIn("1.0.1", current_version)
-      '';
-    };
+      current_version = machine.succeed("grep IMAGE_VERSION /etc/os-release")
+      t.assertIn("1.0.1", current_version)
+    '';
+  };
 }
