6f80a2770b440f7cbc33959b5a973410274f87b2
Samuel Dionne-Riel <samuel.dionne-riel@cyberus-technology.de>
2026-09-18 22:50:15 -0400

    checks/image: Always test both inplaceBootable variants
    Understanding which variants were affected by changes to
    `inplaceBootable` was getting too hard to follow.
    
    Instead, generate both all the time. It should work. If it doesn't,
    something isn't right.

--- checks/image.nix
+++ checks/image.nix
@@ -1,7 +1,49 @@
 { pkgs, nixosModules }:
 
 let
-  mkImageTest = pkgs.callPackage ./image-base.nix ({ inherit nixosModules; });
+  inherit (pkgs) lib;
+  mkImageTest' = pkgs.callPackage ./image-base.nix ({ inherit nixosModules; });
+
+  mkImageTest =
+    attrName:
+    {
+      additionalConfig ? { },
+      name,
+      ...
+    }@args:
+    [
+      {
+        name = "${attrName}Minimal";
+        value = mkImageTest' (
+          args
+          // {
+            name = "${name} (Minimal)";
+            additionalConfig = {
+              imports = [ additionalConfig ];
+              config = {
+                cyberus-linux.image.inplaceBootable = lib.mkForce false;
+              };
+            };
+            additionalImagePrep = growImage;
+          }
+        );
+      }
+      {
+        name = "${attrName}Full";
+        value = mkImageTest' (
+          args
+          // {
+            name = "${name} (Full)";
+            additionalConfig = {
+              imports = [ additionalConfig ];
+              config = {
+                cyberus-linux.image.inplaceBootable = lib.mkForce true;
+              };
+            };
+          }
+        );
+      }
+    ];
 
   # Simulate dd'ing the image to a larger block device.
   growImage = ''
@@ -15,120 +57,100 @@ let
     ])
   '';
 in
-rec {
-  imageDefault = mkImageTest {
-    name = "Image Test (defaults)";
+builtins.listToAttrs (
+  builtins.concatLists [
+    (mkImageTest "imageDefault" {
+      name = "Image Test (defaults)";
+      testScript = ''
+        # Header plus one swap entry
+        print(machine.execute('cat /proc/swaps'))
+        machine.succeed('[ "$(wc -l < /proc/swaps)" -eq 2 ]')
+      '';
+    })
 
-    testScript = ''
-      # Header plus one swap entry
-      print(machine.execute('cat /proc/swaps'))
-      machine.succeed('[ "$(wc -l < /proc/swaps)" -eq 2 ]')
-    '';
-  };
-
-  imageWithoutSwap = mkImageTest {
-    name = "Image Test (disabled swap)";
-
-    additionalConfig = {
-      cyberus-linux.image.swap.enable = false;
-    };
-    testScript = ''
-      # Header without swap entries
-      machine.succeed('[ "$(wc -l < /proc/swaps)" -eq 1 ]')
-    '';
-  };
-
-  imageMinimal = mkImageTest {
-    name = "Image Test (minimized)";
-
-    additionalConfig = {
-      cyberus-linux.image.inplaceBootable = false;
-    };
-
-    additionalImagePrep = growImage;
-
-    testScript = ''
-      # We manage to create a swap partition.
-      machine.succeed('[ "$(wc -l < /proc/swaps)" -eq 2 ]')
-    '';
-  };
-
-  imageMinimalBootDev = mkImageTest {
-    name = "Image Test (minimized, rootdev known)";
-
-    additionalConfig = {
-      cyberus-linux.image.inplaceBootable = false;
-      cyberus-linux.image.bootDevice = "/dev/vda";
-    };
-
-    additionalImagePrep = growImage;
-
-    testScript = ''
-      # We manage to create a swap partition.
-      machine.succeed('[ "$(wc -l < /proc/swaps)" -eq 2 ]')
-    '';
-  };
-
-  imageUpdates = mkImageTest {
-    name = "Image Update Test";
-
-    additionalConfig =
-      {
-        lib,
-        config,
-        extendModules,
-        ...
-      }:
-      {
-        cyberus-linux.image.version = "1.0.0";
-        # FIXME: The shared directory is not mounted, so we cannot use copy_from_host.
-        # We work around this by bundling the image in the generated image.
-        environment.etc.updates =
-          # Only bundle the image in the "outer" system.
-          # If we don't, this `extendModules` also would include an image,
-          # which also would [...] leading to an infinite recursion.
-          lib.mkIf (config.cyberus-linux.image.version == "1.0.0") {
-            source =
-              (extendModules {
-                modules = [
-                  {
-                    cyberus-linux.image.version = lib.mkForce "1.0.1";
-                  }
-                ];
-              }).config.system.build.imageUpdateBundle;
-          };
+    (mkImageTest "imageWithoutSwap" {
+      name = "Image Test (disabled swap)";
+      additionalConfig = {
+        cyberus-linux.image.swap.enable = false;
       };
+      testScript = ''
+        # Header without swap entries
+        machine.succeed('[ "$(wc -l < /proc/swaps)" -eq 1 ]')
+      '';
+    })
 
-    testScript = ''
-      machine.succeed("mkdir -p /var/updates")
+    (mkImageTest "imageKnownBootDev" {
+      name = "Image Test (rootdev known)";
+      additionalConfig = {
+        cyberus-linux.image.bootDevice = "/dev/vda";
+      };
+      testScript = ''
+        # Header without swap entries
+        machine.succeed('[ "$(wc -l < /proc/swaps)" -eq 2 ]')
+      '';
+    })
 
-      # Make the bundled image available.
-      machine.succeed("rm -rf /var/updates")
-      machine.succeed("ln -sf /etc/updates /var/updates")
+    (mkImageTest "imageUpdates" {
+      name = "Image Test (updates)";
 
-      current_version = machine.succeed("grep IMAGE_VERSION /etc/os-release")
-      t.assertIn("1.0.0", current_version)
+      additionalConfig =
+        {
+          lib,
+          config,
+          extendModules,
+          ...
+        }:
+        {
+          cyberus-linux.image.version = "1.0.0";
+          # FIXME: The shared directory is not mounted, so we cannot use copy_from_host.
+          # We work around this by bundling the image in the generated image.
+          environment.etc.updates =
+            # Only bundle the image in the "outer" system.
+            # If we don't, this `extendModules` also would include an image,
+            # which also would [...] leading to an infinite recursion.
+            lib.mkIf (config.cyberus-linux.image.version == "1.0.0") {
+              source =
+                (extendModules {
+                  modules = [
+                    {
+                      cyberus-linux.image.version = lib.mkForce "1.0.1";
+                    }
+                  ];
+                }).config.system.build.imageUpdateBundle;
+            };
+        };
 
-      updates = machine.succeed("updatectl check")
-      t.assertIn("1.0.0 → 1.0.1", updates)
+      testScript = ''
+        machine.succeed("mkdir -p /var/updates")
 
-      # Ensure the update process has completed running.
-      # NOTE: This command likely may not fail on update failures.
-      machine.succeed("updatectl update")
+        # Make the bundled image available.
+        machine.succeed("rm -rf /var/updates")
+        machine.succeed("ln -sf /etc/updates /var/updates")
 
-      # For some failure modes, the `updatectl update` command will exit(0)
-      # Additionally, it will print confusing output such as:
-      #     host@1.0.1: ✗ No space left on device
-      #     host@1.0.1: ✓ Already up-to-date
-      # So we need to check the `check` subcommand instead.
-      # This saves a needless reboot in case of failures.
-      output = machine.succeed("updatectl check 2>&1")
-      t.assertIn("No updates available.", output)
+        current_version = machine.succeed("grep IMAGE_VERSION /etc/os-release")
+        t.assertIn("1.0.0", current_version)
 
-      machine.reboot()
+        updates = machine.succeed("updatectl check")
+        t.assertIn("1.0.0 → 1.0.1", updates)
 
-      current_version = machine.succeed("grep IMAGE_VERSION /etc/os-release")
-      t.assertIn("1.0.1", current_version)
-    '';
-  };
-}
+        # Ensure the update process has completed running.
+        # NOTE: This command likely may not fail on update failures.
+        machine.succeed("updatectl update")
+
+        # For some failure modes, the `updatectl update` command will exit(0)
+        # Additionally, it will print confusing output such as:
+        #     host@1.0.1: ✗ No space left on device
+        #     host@1.0.1: ✓ Already up-to-date
+        # So we need to check the `check` subcommand instead.
+        # This saves a needless reboot in case of failures.
+        output = machine.succeed("updatectl check 2>&1")
+        t.assertIn("No updates available.", output)
+
+        machine.reboot()
+
+        current_version = machine.succeed("grep IMAGE_VERSION /etc/os-release")
+        t.assertIn("1.0.1", current_version)
+      '';
+    })
+  ]
+)
